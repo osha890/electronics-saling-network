65a2f1da457c398003a1eba52bdbd4c483f51876
osha890 <oshavshav@gmail.com>
2025-09-28 21:42:25 +0300

    create Product model

--- esn/products/models.py
+++ esn/products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} {self.model}"
