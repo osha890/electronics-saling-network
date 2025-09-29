319a0ac6af379cf06e5c65ee1a675640a429f4a4
osha890 <oshavshav@gmail.com>
2025-09-29 10:55:42 +0300

    add supplier_link to NetworkNodeAdmin

--- esn/network/admin.py
+++ esn/network/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from network.models import NetworkNode
 
 
 @admin.register(NetworkNode)
 class NetworkNodeAdmin(admin.ModelAdmin):
-    list_display = ("name", "type", "supplier", "debt_to_supplier", "created_at")
+    list_display = ("name", "type", "supplier_link", "debt_to_supplier", "created_at")
+    raw_id_fields = ("supplier",)
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         qs = qs.select_related("supplier")
         return qs
+
+    @admin.display(description="Supplier", ordering="supplier__name")
+    def supplier_link(self, obj):
+        if obj.supplier_id:
+            url = reverse("admin:network_networknode_change", args=[obj.supplier_id])
+            return format_html('<a href="{}">{}</a>', url, obj.supplier.name)
+        return "—"
