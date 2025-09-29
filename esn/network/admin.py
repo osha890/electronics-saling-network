812499df548242f434a297255513abefbcd077ef
osha890 <oshavshav@gmail.com>
2025-09-29 11:14:55 +0300

    add city filter

--- esn/network/admin.py
+++ esn/network/admin.py
@@ -7,12 +7,19 @@ from network.models import NetworkNode
 
 @admin.register(NetworkNode)
 class NetworkNodeAdmin(admin.ModelAdmin):
-    list_display = ("name", "type", "supplier_link", "debt_to_supplier", "created_at")
+    list_display = (
+        "name",
+        "type",
+        "supplier_link",
+        "debt_to_supplier",
+        "get_city",
+    )
     raw_id_fields = ("supplier",)
+    list_filter = ("contact__address__city",)
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        qs = qs.select_related("supplier")
+        qs = qs.select_related("supplier", "contact__address")
         return qs
 
     @admin.display(description="Supplier", ordering="supplier__name")
@@ -21,3 +28,11 @@ class NetworkNodeAdmin(admin.ModelAdmin):
             url = reverse("admin:network_networknode_change", args=[obj.supplier_id])
             return format_html('<a href="{}">{}</a>', url, obj.supplier.name)
         return "—"
+
+    def get_city(self, obj):
+        try:
+            return obj.contact.address.city
+        except AttributeError:
+            return "—"
+
+    get_city.short_description = "City"
