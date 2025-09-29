d762c6f8da2d4986854e765fc23c84a78422a2bf
osha890 <oshavshav@gmail.com>
2025-09-29 14:04:28 +0300

    install django-filter + configure

--- esn/esn/settings.py
+++ esn/esn/settings.py
@@ -129,7 +129,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
+}
 
 if DEBUG:
     INSTALLED_APPS += [
