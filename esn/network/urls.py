ef5dcb07066e81d70b41b7af1b42057cec81f697
osha890 <oshavshav@gmail.com>
2025-09-29 14:42:54 +0300

    create NetworkNodeHighDebtView

--- esn/network/urls.py
+++ esn/network/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from network.views import NetworkNodeReadOnlyViewSet
+from network.views import NetworkNodeHighDebtView, NetworkNodeReadOnlyViewSet
 
 router = DefaultRouter()
 router.register(r"network_nodes", NetworkNodeReadOnlyViewSet, basename="network_node")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path(
+        "network_nodes/high-debt/", NetworkNodeHighDebtView.as_view(), name="high-debt"
+    ),
+] + router.urls
--- esn/network/views.py
+++ esn/network/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
 
 from network.filters import NetworkNodeFilter
 from network.models import NetworkNode
@@ -16,3 +18,19 @@ class NetworkNodeReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
 
     filter_backends = [DjangoFilterBackend]
     filterset_class = NetworkNodeFilter
+
+
+class NetworkNodeHighDebtView(generics.ListAPIView):
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeOutputSerializer
+
+    def list(self, request, *args, **kwargs):
+        avg_debt = NetworkNode.objects.exclude(
+            supplier=None,
+        ).aggregate(
+            avg=Avg("debt_to_supplier"),
+        )["avg"]
+        queryset = self.get_queryset().filter(debt_to_supplier__gt=avg_debt)
+        serializer = self.get_serializer(queryset, many=True)
+        data = {"average_debt": avg_debt, "nodes": serializer.data}
+        return Response(data)
