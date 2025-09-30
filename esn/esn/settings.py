aa9a5620bbeeb53bc9d63a8cba5185ba93117ac6
osha890 <oshavshav@gmail.com>
2025-09-30 15:35:28 +0300

    configure email backend + envs

--- esn/esn/settings.py
+++ esn/esn/settings.py
@@ -154,6 +154,15 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+
+
 if DEBUG:
     INSTALLED_APPS += [
         "debug_toolbar",
